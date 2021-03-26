8d28f8e31760bf02affc1a7a30a991141321a162
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-03-26 15:43:37 +0200

    Added routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
+import WithRestoService from '../hoc';
+import {Route, Switch} from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
-const App = () => {
+const App = ({RestoService}) => {
+    console.log(RestoService.getMenuItems());
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <Switch>
+                <Route exact path='/' component={MainPage} />
+                <Route exact path='/cart' component={CartPage} />
+                <Route exact component={MainPage} />
+            </Switch>
         </div>
     )
 }
 
-export default App;
+export default WithRestoService()(App);
