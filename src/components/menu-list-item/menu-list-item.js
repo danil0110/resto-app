4adc2ebdfa90949270fa9a4b47ecb1fc6c850dae
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-03-26 16:52:29 +0200

    Added icons for categories

--- src/components/menu-list-item/menu-list-item.js
+++ src/components/menu-list-item/menu-list-item.js
@@ -3,11 +3,24 @@ import './menu-list-item.scss';
 
 const MenuListItem = ({menuItem}) => {
     const {title, price, url, category} = menuItem;
+    const icon = (() => {
+        switch (category) {
+            case 'pizza':
+                return 'pizza-slice';
+            case 'meat':
+                return 'drumstick-bite';
+            case 'salads':
+                return 'seedling';
+            default:
+                return 'utensils';
+        }
+    })();
+
     return (
         <li className="menu__item">
             <div className="menu__title">{title}</div>
             <img className="menu__img" src={url} alt={title}></img>
-            <div className="menu__category">Category: <span>{category}</span></div>
+            <div className="menu__category">Category: <span>{category} <i className={`fas fa-${icon}`}></i></span></div>
             <div className="menu__price">Price: <span>{price}$</span></div>
             <button className="menu__btn">Add to cart</button>
         </li>
